c672d522a02931151f57b41e48e69bd42e002e30
Daniel Gaytan <dgaytan6@gmail.com>
2014-09-18 12:34:20 -0500

    Remove fullpage.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,1 @@
 //= require jquery
-//= require fullpage.js/jquery.fullPage.js
-//= require fullpage.js/vendors/jquery.easings.min
-
-$(function(){
-  $('#content').fullpage({
-    menu: '.js-fullpage-menu',
-    anchors: ['home', 'topics', 'location', 'cfp']
-  });
-})
